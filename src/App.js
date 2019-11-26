d0747a43abfb04684476180f4fb794ff59e9ef31
ramiasg <rami.sweyri@gmail.com>
2019-11-26 22:53:20 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/Navbar';
+import Slider from './components/Slider';
+import Product from './components/Product';
+import location from './components/location';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Slider />
+      <Product />
+      <location />
+      <Contact />
+      <Footer />
     </div>
   );
 }
